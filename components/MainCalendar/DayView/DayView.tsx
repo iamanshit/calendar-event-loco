576d85b9990cbb81be7fd63c66b6bbb0b422a095
iamanshit <me@anshitchaudhary.com>
2025-02-17 03:57:56 +0530

    Add individual day view sub component

---
+++ components/MainCalendar/DayView/DayView.tsx
@@ -0,0 +1,23 @@
+import * as dayjs from "dayjs";
+import styles from "./DayView.module.scss";
+
+export default function DayView({ day, rInd }) {
+  return (
+    <div className={styles.day}>
+      {rInd === 0 && <h4>{day.format("ddd").toUpperCase()}</h4>}
+      <p
+        className={
+          day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+            ? styles.highlight
+            : ""
+        }
+      >
+        {day.date() === 1 ? day.format("MMM D") : day.format("D")}
+      </p>
+      <div className={styles.event}>
+        <p>event name</p>
+        <p>event name</p>
+      </div>
+    </div>
+  );
+}
