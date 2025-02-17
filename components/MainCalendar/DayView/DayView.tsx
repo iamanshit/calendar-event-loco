3531c59993111c248a4f952303650bbb217dde91
iamanshit <me@anshitchaudhary.com>
2025-02-17 14:55:07 +0530

    Complete event add functionality

--- components/MainCalendar/DayView/DayView.tsx
+++ components/MainCalendar/DayView/DayView.tsx
@@ -1,21 +1,33 @@
 import * as dayjs from "dayjs";
+import calendarStore from "../../../app/store/calendarStore";
 import styles from "./DayView.module.scss";
+
 export default function DayView({ day, rInd, handleClick }) {
+  const { events } = calendarStore();
   return (
     <li className={styles.day} onClick={handleClick}>
       {rInd === 0 && <h4>{day.format("ddd").toUpperCase()}</h4>}
-      <p
-        className={
-          day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
-            ? styles.highlight
-            : ""
-        }
-      >
-        {day.date() === 1 ? day.format("MMM D") : day.format("D")}
-      </p>
-      <div className={styles.event}>
-        <p>event name</p>
-        <p>event name</p>
+      <div className={styles.innerWrapper}>
+        <p
+          className={
+            day.format("DD-MM-YY") === dayjs().format("DD-MM-YY")
+              ? styles.highlight
+              : ""
+          }
+        >
+          {day.date() === 1 ? day.format("MMM D") : day.format("D")}
+        </p>
+        <>
+          {events && events.length > 0 && (
+            <div className={styles.event}>
+              {events.map((event, id) => {
+                if (event.date === day.format("DD/MM/YYYY")) {
+                  return <p key={id}>{event.title.slice(0, 10)}</p>;
+                }
+              })}
+            </div>
+          )}
+        </>
       </div>
     </li>
   );
